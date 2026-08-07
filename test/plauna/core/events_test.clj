923bba5c150f2926c6edbc9199c5d38f068a5add
SvenPe <sven@svenpeters.net>
2026-08-07 22:49:57 +0200

    Stabilize asynchronous event loop tests

--- test/plauna/core/events_test.clj
+++ test/plauna/core/events_test.clj
@@ -3,6 +3,16 @@
             [clojure.core.async :as async]
             [clojure.test :as test]))
 
+(defn- await-count
+  "Wait for an asynchronous worker count without relying on a fixed scheduler delay."
+  [counter expected]
+  (let [deadline (+ (System/nanoTime) 2000000000)]
+    (loop []
+      (cond
+        (= expected @counter) true
+        (< (System/nanoTime) deadline) (do (Thread/sleep 10) (recur))
+        :else false))))
+
 (test/deftest return-key-on-complete-works
   (let [test-chan (async/chan)
         test-fn (fn [] (async/go (async/<! test-chan)))
@@ -15,23 +25,35 @@
         test-chan (atom (async/chan))
         test-fn (fn [] (async/go (swap! test-atom inc) (async/<! @test-chan)))
         test-register {:test-case test-fn}]
-    (events/keep-track {:test-case (events/return-key-on-complete :test-case test-fn)} test-register)
-    (swap! test-chan (fn [old] (async/close! old) (async/chan)))
-    (Thread/sleep 100)
-    (swap! test-chan (fn [old] (async/close! old) (async/chan)))
-    (Thread/sleep 100)
-    (test/is (= 3 @test-atom))))
+    (let [supervisor (events/keep-track
+                      {:test-case (events/return-key-on-complete :test-case test-fn)}
+                      test-register)]
+      (try
+        (test/is (await-count test-atom 1))
+        (swap! test-chan (fn [old] (async/close! old) (async/chan)))
+        (test/is (await-count test-atom 2))
+        (swap! test-chan (fn [old] (async/close! old) (async/chan)))
+        (test/is (await-count test-atom 3))
+        (finally
+          (events/stop-event-loops!)
+          (async/close! @test-chan)
+          (async/<!! supervisor))))))
 
 (test/deftest event-register-works
   (let [test-atom (atom 0)
         test-chan (atom (async/chan))
         test-fn (fn [] (async/go (swap! test-atom inc) (async/<! @test-chan)))
         test-register {:test-case test-fn}]
-    (events/start-event-loops test-register)
-    (swap! test-chan (fn [old] (async/close! old) (async/chan)))
-    (Thread/sleep 100)
-    (swap! test-chan (fn [old] (async/close! old) (async/chan)))
-    (Thread/sleep 100)
-    (swap! test-chan (fn [old] (async/close! old) (async/chan)))
-    (Thread/sleep 100)
-    (test/is (= 3 @test-atom))))
+    (let [supervisor (events/start-event-loops test-register)]
+      (try
+        (test/is (await-count test-atom 1))
+        (swap! test-chan (fn [old] (async/close! old) (async/chan)))
+        (test/is (await-count test-atom 2))
+        (swap! test-chan (fn [old] (async/close! old) (async/chan)))
+        (test/is (await-count test-atom 3))
+        (swap! test-chan (fn [old] (async/close! old) (async/chan)))
+        (test/is (await-count test-atom 4))
+        (finally
+          (events/stop-event-loops!)
+          (async/close! @test-chan)
+          (async/<!! supervisor))))))
